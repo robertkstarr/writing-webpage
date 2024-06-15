21552e2d5f1e30d526a34f1fa25a464b6d856210
Robert Starr <robert.k.starr@gmail.com>
2024-06-15 10:41:22 -0700

    Minor adjustments to design

--- src/Desktop/DesktopHeader.tsx
+++ src/Desktop/DesktopHeader.tsx
@@ -1,14 +1,21 @@
 import { AppBar, Box, Button, Toolbar } from "@mui/material";
 
-import React from "react";
-
 const pages = ["Products", "Pricing", "Blog"];
 
 const DesktopHeader = () => {
   return (
     <AppBar position="fixed">
-      <Toolbar>
-        <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+      <Toolbar sx={{ justifyContent: "center" }}>
+        <Box
+          sx={{
+            flexGrow: 1,
+            display: {
+              xs: "flex",
+              justifyContent: "space-between",
+              maxWidth: "50rem",
+            },
+          }}
+        >
           {pages.map((page) => (
             <Button
               key={page}
