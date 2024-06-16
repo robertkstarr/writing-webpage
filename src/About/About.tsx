177215115f812ae36bd706ef815d3d29f5fe1182
Robert Starr <robert.k.starr@gmail.com>
2024-06-15 18:01:36 -0700

    Added gh-pages

--- src/About/About.tsx
+++ src/About/About.tsx
@@ -1,39 +1,57 @@
-import { Box, Card, CardMedia } from "@mui/material";
+import { Box, Card, CardMedia, Container, Typography } from "@mui/material";
 import portrait from "../assets/portrait.jpg";
 import AboutText from "./AboutText";
 
 const About = () => {
   return (
-    <>
-      <Card
+    <Container
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        height: "100%",
+        margin: 0,
+      }}
+    >
+      {/* <Box> */}
+      <Typography variant="h3">About</Typography>
+      {/* </Box> */}
+      <Container
         sx={{
           display: "flex",
-          padding: 1,
-          flexGrow: "1",
-          flexDirection: "column",
-          justifyContent: "center",
-          backgroundColor: "inherit",
-          backgroundImage: "none",
+          flexDirection: "row",
+          height: "100%",
         }}
       >
-        <CardMedia component="img" height="70%" image={portrait} />
-      </Card>
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          flexGrow: "2",
-          width: "80%",
-          margin: 4,
-          textAlign: "left",
-          gap: 3,
-          overflowY: "auto",
-          justifyContent: "center",
-        }}
-      >
-        <AboutText />
-      </Box>
-    </>
+        <Card
+          sx={{
+            display: "flex",
+            padding: 1,
+            flexGrow: "1",
+            flexDirection: "column",
+            justifyContent: "center",
+            backgroundColor: "inherit",
+            backgroundImage: "none",
+          }}
+        >
+          <CardMedia component="img" height="40%" image={portrait} />
+        </Card>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            flexGrow: "2",
+            width: "80%",
+            margin: 4,
+            textAlign: "left",
+            gap: 3,
+            overflowY: "auto",
+            justifyContent: "center",
+          }}
+        >
+          <AboutText />
+        </Box>
+      </Container>
+    </Container>
   );
 };
 
