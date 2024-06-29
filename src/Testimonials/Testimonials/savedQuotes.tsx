4d4f874d8492aa50fea852543594ad15cd113b42
Robert Starr <robert.k.starr@gmail.com>
2024-06-29 11:54:41 -0700

    Updated testimonials.

--- src/Testimonials/Testimonials/savedQuotes.tsx
+++ src/Testimonials/Testimonials/savedQuotes.tsx
@@ -16,16 +16,38 @@ const Eliana = () => {
   const testimonial =
     "Working with Robert Starr is an absolute delight. His ability to write compelling characters, create intricate plots, and his consistency in delivering page-turning quality stories is remarkable. Beyond his remarkable writing, collaborating with Robert Starr is an absolute pleasure. He is diligent in meeting deadlines, and his rewriting skills are outstanding! His incredibly easy-going personality, and passion for the craft make every project enjoyable and productive. I highly recommend his work and collaboration to anyone looking to work with an outstanding  ghostwriter.";
 
-  const source = "Eliana";
+  const source = "Eliana Dell'Acqua";
+
+  const company = "Editor";
+
+  return <Quote testimonial={testimonial} source={source} company={company} />;
+};
+const Upwork1 = () => {
+  const testimonial =
+    "Robert was awesome to work with and delivered amazing quality!";
+
+  const source = "Upwork Client";
 
   return <Quote testimonial={testimonial} source={source} />;
 };
 
+const Upwork2 = () => {
+  const testimonial =
+    "Robert was an excellent writer to work with, very cooperative, and very willing to adapt to our writing style. He has a gift for telling stories that are compelling. We'd love to work with him again in the future.";
+
+  const source = "Panagiotis Soultanos";
+
+  const company = "Upwork Client";
+
+  return <Quote testimonial={testimonial} source={source} company={company} />;
+};
 const SavedQuotes = () => {
   return (
     <React.Fragment>
       <MikeMartins />
       <Eliana />
+      <Upwork1 />
+      <Upwork2 />
     </React.Fragment>
   );
 };
