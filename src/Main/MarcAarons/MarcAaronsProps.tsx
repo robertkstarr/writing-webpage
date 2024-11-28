6e3a3914165225fe4c062595a056c69226c648e5
Robert Starr <robert.k.starr@gmail.com>
2024-11-27 20:16:36 -0800

    Fixed formatting

--- src/Main/MarcAarons/MarcAaronsProps.tsx
+++ src/Main/MarcAarons/MarcAaronsProps.tsx
@@ -1,3 +1,4 @@
+import Box from "@mui/material/Box";
 import LoremIpsum from "react-lorem-ipsum";
 import About from "../../About/About";
 import portrait from "../../assets/dragon.jpg";
@@ -15,7 +16,12 @@ export default {
   theme: HackerTheme,
 
   content: (
-    <>
+    <Box
+      width="100%"
+      justifyContent={"center"}
+      display={"flex"}
+      flexDirection={"column"}
+    >
       <SiteHeader
         navDropDownMenu={
           <NavDropDownMenu navigationOptions={NavigationOptions} />
@@ -26,7 +32,7 @@ export default {
       <Services pageContent={<LoremIpsum p={3} />} />
       <Contact />
       <SiteFooter author={"Marc Aarons"} />
-    </>
+    </Box>
   ),
 
   title: "Marc Aarons - M/M Romance Author",
